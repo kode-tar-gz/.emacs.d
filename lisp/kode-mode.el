a4fd92321f6f87cfdc812177469848747667e9fb
Kode-Kun <lui.cardoso.1@ulaval.ca>
2026-05-07 15:36:42 -0400

    removed trailing white space

--- lisp/kode-mode.el
+++ lisp/kode-mode.el
@@ -1,6 +1,6 @@
 ;; My custom minor mode, mainly used for overriding keybindings.
 ;; DISCLAIMER: A lot of my custom functions in this file are
-;; made with AI (gemini, mostly). 
+;; made with AI (gemini, mostly).
 
 (require 'move-text)
 (require 'rust-mode)
